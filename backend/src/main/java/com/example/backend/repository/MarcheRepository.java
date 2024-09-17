c0bffb6060df0ad1a1ed2cd7ffb74cd6c4df1233
Yahya Zini <zini.y201@gmail.com>
2024-09-17 20:06:45 +0100

    affaire start look good

--- backend/src/main/java/com/example/backend/repository/MarcheRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.Marche;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MarcheRepository extends JpaRepository<Marche, Long> {
-}
